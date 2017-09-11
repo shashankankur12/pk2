d60413ff8e28a0d7c6335853a2f5053fe358829b
OBITO <shashankankur12@gmail.com>
2017-09-11 16:45:12 +0530

    Initial commit

---
+++ app/src/main/java/aavartan/pocketkharch/Splash.java
@@ -0,0 +1,40 @@
+package aavartan.pocketkharch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+
+    //private TextView textView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //textView=(TextView) findViewById(R.id.textView);
+        imageView=(ImageView) findViewById(R.id.imageView);
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.animation_page);
+        imageView.startAnimation(animation);
+        //Typeface mycustomefont=Typeface.createFromAsset(getAssets(),"font/segoepr.ttf");
+        //textView.setTypeface(mycustomefont);
+        //final Intent intent=new Intent(this,);
+        Thread timer =new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    //startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
